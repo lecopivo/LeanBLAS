687442541aceefb054bad71f8e1dbdd386a1fab4
lecopivo <skrivantomas@seznam.cz>
2025-02-17 17:01:27 -0500

    add missing file

---
+++ LeanBLAS/CBLAS/LevelOneFloat64Spec.lean
@@ -0,0 +1,54 @@
+import LeanBLAS.ComplexFloat
+import LeanBLAS.Spec.LevelOne
+import LeanBLAS.CBLAS.LevelOneFloat64
+
+namespace BLAS.CBLAS
+
+
+
+
+instance : LevelOneData FloatArray Float Float where
+  size x := x.size
+  get x i := x.get! i
+  set x i v := x.set! i v
+  ofFn {n} f := Id.run do
+    let mut x : FloatArray := FloatArray.mkEmpty n
+    for h : i in [0:n] do
+      let i : Fin n := ⟨i, h.2.1⟩
+      x := x.push (f i)
+    x
+  dot N X offX incX Y offY incY := ddot N.toUSize X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize
+  nrm2 N X offX incX := dnrm2 N.toUSize X offX.toUSize incX.toUSize
+  asum N X offX incX := dasum N.toUSize X offX.toUSize incX.toUSize
+  iamax N X offX incX := idamax N.toUSize X offX.toUSize incX.toUSize |>.toNat
+  swap N X offX incX Y offY incY := dswap N.toUSize X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize
+  copy N X offX incX Y offY incY := dcopy N.toUSize X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize
+  axpy N a X offX incX Y offY incY := daxpy N.toUSize a X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize
+  rotg a b := drotg a b
+  rotmg d1 d2 b1 b2 := drotmg d1 d2 b1 b2
+  rot N X offX incX Y offY incY c s := drot N.toUSize X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize c s
+  scal N a X offX incX := dscal N.toUSize a X offX.toUSize incX.toUSize
+
+
+
+set_option linter.unusedVariables false in
+instance : LevelOneDataExt FloatArray Float Float where
+  const N a := dconst N.toUSize a
+  sum N X offX incX := dsum N.toUSize X offX.toUSize incX.toUSize
+  axpby N a X offX incX b Y offY incY := daxpby N.toUSize a X offX.toUSize incX.toUSize b Y offY.toUSize incY.toUSize
+  scaladd N a X offX incX b := dscaladd N.toUSize a X offX.toUSize incX.toUSize b
+
+  imaxRe N X offX incX h := (dimaxRe N.toUSize X offX.toUSize incX.toUSize).toNat
+  imaxIm N X offX incX h := offX
+  iminRe N X offX incX h := (diminRe N.toUSize X offX.toUSize incX.toUSize).toNat
+  iminIm N X offX incX h := offX
+
+  mul N X offX incX Y offY incY := dmul N.toUSize X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize
+  div N X offX incX Y offY incY := ddiv N.toUSize X offX.toUSize incX.toUSize Y offY.toUSize incY.toUSize
+  inv N X offX incX := dinv N.toUSize X offX.toUSize incX.toUSize
+  abs N X offX incX := dabs N.toUSize X offX.toUSize incX.toUSize
+  sqrt N X offX incX := dsqrt N.toUSize X offX.toUSize incX.toUSize
+  exp N X offX incX := dexp N.toUSize X offX.toUSize incX.toUSize
+  log N X offX incX := dlog N.toUSize X offX.toUSize incX.toUSize
+  sin N X offX incX := dsin N.toUSize X offX.toUSize incX.toUSize
+  cos N X offX incX := dcos N.toUSize X offX.toUSize incX.toUSize
