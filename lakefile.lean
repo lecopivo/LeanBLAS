e5a0e4d5341b69cb5cf4937d43aa4c7b68be5035
lecopivo <skrivantomas@seznam.cz>
2025-02-17 15:51:14 -0500

    try building blas during setting up build

--- lakefile.lean
+++ lakefile.lean
@@ -100,6 +100,7 @@ target libopenblas pkg : FilePath := do
 
 extern_lib libleanblasc pkg := do
   let openblas ← libopenblas.fetch
+  let _ ← openblas.await -- hack :(
   let inclArgs := #[s!"-I{pkg.lakeDir / "build" / "OpenBLAS"}"]
 
   let mut oFiles : Array (Job FilePath) := #[]
