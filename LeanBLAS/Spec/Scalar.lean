44d5a9401f8f9bd44b152b0f8069f249114b5287
lecopivo <skrivantomas@seznam.cz>
2025-02-17 12:53:10 -0500

    add missing file

---
+++ LeanBLAS/Spec/Scalar.lean
@@ -0,0 +1,30 @@
+import Mathlib.Analysis.RCLike.Basic
+import Mathlib.Data.Complex.Basic
+
+import LeanBLAS.Util
+
+namespace BLAS
+
+open Sorry RCLike
+
+local notation "𝓚" => algebraMap ℝ _
+
+def toReal {𝕜} [RCLike 𝕜] (x : 𝕜) : ℝ := re x
+
+noncomputable
+def fromReal (𝕜 : Type*) [RCLike 𝕜] (x : ℝ) : 𝕜 := 𝓚 (re x)
+
+def toComplex {𝕜} [RCLike 𝕜] (x : 𝕜) : ℂ := ⟨re x, im x⟩
+
+noncomputable
+def fromComplex (𝕜 : Type*) [RCLike 𝕜] (x : ℂ) : 𝕜 := 𝓚 (x.re) + 𝓚 (x.im) * I
+
+variable {𝕜} [RCLike 𝕜]
+
+@[simp]
+theorem fromComplex_toComplex (x : 𝕜) : fromComplex 𝕜 (toComplex x) = x := by
+  simp[fromComplex,toComplex]
+
+@[simp]
+theorem toReal_fromReal (x : ℝ) : toReal (fromReal 𝕜 x) = x := by
+  simp[fromReal,toReal]
